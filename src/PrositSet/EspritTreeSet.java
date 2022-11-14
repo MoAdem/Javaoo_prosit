281f69f65815223ba4da21213c06dd31fc7c65cb
MoAdem <mohamedadem.torkhani@esprit.tn>
2022-11-14 12:47:00 +0100

    set

--- src/PrositSet/EspritTreeSet.java
+++ src/PrositSet/EspritTreeSet.java
@@ -1,16 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package PrositSet;
 
-import java.util.HashSet;
 import java.util.TreeSet;
 
-/**
- *
- * @author MDJMi
- */
 public class EspritTreeSet implements GestionEnseignant {
 
     TreeSet <Enseignant> tset ;
